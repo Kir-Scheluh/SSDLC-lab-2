5616e5e3b18e1a8e9bcc0288017d2da1b45e97e1
Кирилл Щелухин <71601556+Kir-Scheluh@users.noreply.github.com>
2023-11-20 14:36:01 +0300

    Add project files.

---
+++ ConsoleApp1/BruteForce.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class BruteForce
+    {
+        static char[] alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+
+        static public string calculateSHA256(string input)
+        {
+            // Создать объект класса SHA256
+            using (var sha256 = SHA256.Create())
+            {
+                // Преобразовать входную строку в байтовый массив
+                var inputBytes = Encoding.UTF8.GetBytes(input);
+
+                // Вычислить хэш SHA-256
+                var hashBytes = sha256.ComputeHash(inputBytes);
+
+                // Преобразовать байтовый массив в строку шестнадцатеричных чисел
+                var hashString = BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+
+                // Вернуть строку хэша
+                return hashString;
+            }
+        }
+        static IEnumerable<string> GetPermutations(char[] alphabet, int length)
+        {
+            if (length == 1)
+            {
+                return alphabet.Select(c => c.ToString());
+            }
+
+            return GetPermutations(alphabet, length - 1)
+                .SelectMany(s => alphabet, (s, c) => s + c);
+        }
+
+        static public void bruteForce(int numberOfThreads, List<string> targetValues)
+        {
+            ParallelOptions options = new ParallelOptions { MaxDegreeOfParallelism = numberOfThreads };
+            Parallel.ForEach(GetPermutations(alphabet, 5), options, (password) =>
+            {
+                string sha256 = calculateSHA256(password);
+                foreach (string targetValue in targetValues)
+                {
+                    if (targetValue == sha256)
+                    {
+                        Console.WriteLine($"Найден пароль: {password} соответствующий хеш: {sha256}");
+                        //count++;
+                        //if (count == 3) return;
+                    }
+                }
+            });
+
+            Console.WriteLine("Поиск завершен");
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            List<string> targetValues = new List<string>
+            {
+                //    "3a7bd3e2360a3d29eea436fcfb7e44c735d117c42d1c1835420b6b9942dd4f1b",
+                //    "1115dd800feaacefdf481f1f9070374a2a81e27880f187396db67958b207cbad",
+                //    "74e1bb62f8dabb8125a58852b63bdf6eaef667cb56ac7f7cdba6d7305c50a22f",
+            };
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Введите хэш-значение №{i + 1}");
+                targetValues.Add(Console.ReadLine());
+            }
+            Console.WriteLine("Введите количество потоков");
+            int treadsNumber = Convert.ToInt32(Console.ReadLine());
+
+            stopwatch.Start();
+            BruteForce.bruteForce(treadsNumber, targetValues);
+            stopwatch.Stop();
+
+            TimeSpan elapsedTime = stopwatch.Elapsed;
+
+            Console.WriteLine("Прошло времени: " + elapsedTime);
+        }
+    }
+}
